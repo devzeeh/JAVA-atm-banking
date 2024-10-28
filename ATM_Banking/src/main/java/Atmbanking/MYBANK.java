f88a665fb950def3052b93653b838dc5936585c3
jjohnZeeh1 <jjohnroxas06@gmail.com>
2024-10-28 11:18:35 +0800

    Add dependency for java connector SQL

---
+++ ATM_Banking/src/main/java/Atmbanking/MYBANK.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Atmbanking;
+
+/**
+ *
+ * @author HP
+ */
+public class MYBANK {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
