9b868b69023bca398ab37a874dc0ea915c184bd9
jjohnZeeh1 <jjohnroxas06@gmail.com>
2024-10-28 10:38:10 +0800

    First Commit

---
+++ ATM_Banking/src/main/java/Atmbanking/ATM_Banking.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Atmbanking;
+
+/**
+ *
+ * @author Errol
+ */
+public class ATM_Banking {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
